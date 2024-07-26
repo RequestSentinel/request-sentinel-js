1585dc94a8dc88af0ec08175fb2033b1f7ddd43b
Umar Nizamani <umar.nizamani@gmail.com>
2024-07-26 16:39:34 +0200

    Improved SDK

--- dist/index.js
+++ dist/index.js
@@ -1,58 +1,34 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 var RequestSentinel = /** @class */ (function () {
     function RequestSentinel(API_KEY, appVersion, appEnvironment, options) {
         // const { overrideFetch = true, overrideXMLHTTP = false } = options;
-        console.log('Constructing with key', API_KEY);
         RequestSentinel.API_KEY = API_KEY;
         RequestSentinel.appVersion = appVersion;
         RequestSentinel.appEnvironment = appEnvironment;
-        this.options = options;
+        RequestSentinel.options = options;
         // We add an interceptor for XMLHttpRequest
         if (options.overrideXMLHttpRequest) {
             this._overrideXMLHttpRequest(RequestSentinel.postToAPI);
         }
         // We add an interceptor for fetch
         if (options.overrideFetch) {
-            this._overrideFetch();
+            this._overrideFetch(RequestSentinel.postToAPI);
         }
         if (options.overrideWebSocket) {
-            this._interceptWebSocket();
+            this._interceptWebSocket(RequestSentinel.postToAPI);
         }
+        console.log('Request Sentinel | Initialised');
     }
     RequestSentinel.init = function (API_KEY, appVersion, appEnvironment, options) {
-        if (options === void 0) { options = { overrideFetch: true, overrideXMLHttpRequest: true, overrideWebSocket: true, ignoreBody: true }; }
+        if (options === void 0) { options = { overrideFetch: true, overrideXMLHttpRequest: true, overrideWebSocket: true, ignoreBody: true, debug: true }; }
         return this._instance || (this._instance = new this(API_KEY, appVersion, appEnvironment, options));
     };
     RequestSentinel.postToAPI = function (endpoint, method) {
         // Ignore all requests to RequestSentinel to avoid infinite loop
-        if (endpoint.indexOf('localhost') !== -1) {
+        if (endpoint.indexOf('api.requestsentinel') !== -1) {
             return;
         }
-        var url = "http://localhost:8080/processor/ingest/request/outgoing";
+        var url = "https://api.requestsentinel.com/processor/ingest/request/outgoing";
         var headers = {
             "Content-Type": "application/json",
             "API-KEY": RequestSentinel.API_KEY
@@ -71,36 +47,28 @@ var RequestSentinel = /** @class */ (function () {
             headers: headers,
             body: JSON.stringify(data)
         })
-            .then(function (response) { return response.json(); })
-            .then(function (data) { return console.log('Request Sentinel | Response:', data); })
             .catch(function (error) { return console.error('Request Sentinel | Error:', error); });
     };
-    RequestSentinel.prototype.outgoingFetchRequest = function () {
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
-        }
-        console.log.apply(console, __spreadArray(['Request Sentinel | Outgoing Fetch request:'], args, false));
-    };
-    RequestSentinel.prototype.outgoingXHRRequest = function (method, url, async, username, password) {
-        if (async === void 0) { async = true; }
-        console.log('Request Sentinel | Outgoing XHR request:', { method: method, url: url, async: async, username: username, password: password });
-    };
+    ////// Override Network Functions /////////
     RequestSentinel.prototype._overrideXMLHttpRequest = function (callback) {
         var originalOpen = XMLHttpRequest.prototype.open;
         XMLHttpRequest.prototype.open = function (method, url, async, user, password) {
             if (async === void 0) { async = true; }
-            callback(url, method);
-            console.log("Request Sentinel | [XMLHttpRequest] ".concat(method, " request to ").concat(url));
+            if (callback) {
+                callback(url, method);
+            }
+            if (RequestSentinel.options.debug) {
+                console.log("Request Sentinel | [XMLHttpRequest] ".concat(method, " request to ").concat(url));
+            }
             return originalOpen.apply(this, arguments);
         };
-        var originalSend = XMLHttpRequest.prototype.send;
-        XMLHttpRequest.prototype.send = function (body) {
-            console.log("Request Sentinel | [XMLHttpRequest] Request body: ".concat(body));
-            return originalSend.apply(this, arguments);
-        };
+        // const originalSend = XMLHttpRequest.prototype.send;
+        // XMLHttpRequest.prototype.send = function (body) {
+        //     console.log(`Request Sentinel | [XMLHttpRequest] Request body: ${body}`);
+        //     return originalSend.apply(this, arguments as any);
+        // };
     };
-    RequestSentinel.prototype._overrideFetch = function () {
+    RequestSentinel.prototype._overrideFetch = function (callback) {
         var originalFetch = window.fetch;
         window.fetch = function (input, init) {
             var method = 'GET';
@@ -117,23 +85,33 @@ var RequestSentinel = /** @class */ (function () {
             if (init && init.method) {
                 method = init.method;
             }
-            console.log("Request Sentinel | [fetch] ".concat(method, " request to ").concat(url));
             if (init && init.body) {
-                console.log("Request Sentinel | [fetch] Request body: ".concat(init.body));
+                // console.log(`Request Sentinel | [fetch] Request body: ${init.body}`);
+            }
+            if (callback) {
+                if (RequestSentinel.options.debug) {
+                    console.log("Request Sentinel | [fetch] ".concat(method, " request to ").concat(url));
+                }
+                callback(url, method);
             }
             return originalFetch.apply(this, arguments);
         };
     };
-    RequestSentinel.prototype._interceptWebSocket = function () {
-        var originalWebSocket = window.WebSocket;
-        window.WebSocket = /** @class */ (function (_super) {
-            __extends(class_1, _super);
-            function class_1(url, protocols) {
-                console.log("Request Sentinel | [WebSocket] Connection to ".concat(url));
-                return _super.call(this, url, protocols) || this;
+    RequestSentinel.prototype._interceptWebSocket = function (callback) {
+        var OriginalWebSocket = window.WebSocket;
+        // @ts-ignore: Overriding window.WebSocket
+        window.WebSocket = new Proxy(OriginalWebSocket, {
+            construct: function (target, args) {
+                var url = args[0], protocols = args[1];
+                if (RequestSentinel.options.debug) {
+                    console.log("Request Sentinel | [WebSocket] Connection to ".concat(url));
+                }
+                if (callback) {
+                    callback(url.toString(), 'WEBSOCKET');
+                }
+                return new target(url, protocols);
             }
-            return class_1;
-        }(originalWebSocket));
+        });
     };
     return RequestSentinel;
 }());
--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,7 @@ type RequestSentinelOptions = {
     overrideWebSocket?: boolean;
 
     ignoreBody?: boolean;
+    debug?: boolean;
 };
 
 class RequestSentinel {
@@ -14,25 +15,23 @@ class RequestSentinel {
     static appEnvironment: string;
 
     // Initialise options with default values
-    private options: RequestSentinelOptions;
+    static options: RequestSentinelOptions;
 
     // We use the singleton pattern for Request Sentinel
     private static _instance: RequestSentinel;
 
     static init(API_KEY: string, appVersion:string, appEnvironment: string, options: RequestSentinelOptions 
-        = {overrideFetch: true, overrideXMLHttpRequest: true, overrideWebSocket: true, ignoreBody: true}) {
+        = {overrideFetch: true, overrideXMLHttpRequest: true, overrideWebSocket: true, ignoreBody: true, debug: true}) {
         return this._instance || (this._instance = new this(API_KEY, appVersion, appEnvironment, options));
     }
     
     constructor(API_KEY: string, appVersion:string, appEnvironment: string, options: RequestSentinelOptions) {
-        // const { overrideFetch = true, overrideXMLHTTP = false } = options;
-        console.log('Constructing with key', API_KEY)
 
         RequestSentinel.API_KEY = API_KEY
         RequestSentinel.appVersion = appVersion
         RequestSentinel.appEnvironment = appEnvironment
 
-        this.options = options
+        RequestSentinel.options = options
         
         // We add an interceptor for XMLHttpRequest
         if (options.overrideXMLHttpRequest) {
@@ -41,22 +40,23 @@ class RequestSentinel {
 
         // We add an interceptor for fetch
         if (options.overrideFetch) {
-            this._overrideFetch();
+            this._overrideFetch(RequestSentinel.postToAPI);
         }
 
         if (options.overrideWebSocket) {
-            this._interceptWebSocket();
+            this._interceptWebSocket(RequestSentinel.postToAPI);
         }
+
+        console.log('Request Sentinel | Initialised')
     }
 
     static postToAPI(endpoint: string, method: string) {
-
         // Ignore all requests to RequestSentinel to avoid infinite loop
-        if (endpoint.indexOf('localhost') !== -1) {
+        if (endpoint.indexOf('api.requestsentinel') !== -1) {
             return;
         }
 
-        const url = "http://localhost:8080/processor/ingest/request/outgoing";
+        const url = "https://api.requestsentinel.com/processor/ingest/request/outgoing";
         const headers = {
             "Content-Type": "application/json",
             "API-KEY": RequestSentinel.API_KEY
@@ -71,41 +71,38 @@ class RequestSentinel {
             timestamp: new Date().toISOString(),
             // timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
         };
-    
+
         fetch(url, {
             method: "POST",
             headers: headers,
             body: JSON.stringify(data)
         })
-        .then(response => response.json())
-        .then(data => console.log('Request Sentinel | Response:', data))
         .catch(error => console.error('Request Sentinel | Error:', error));
     }
 
-    outgoingFetchRequest(...args: any[]) {
-        console.log('Request Sentinel | Outgoing Fetch request:', ...args);
-    }
 
-    outgoingXHRRequest(method: string, url: string | URL, async: boolean = true, username?: string, password?: string) {
-        console.log('Request Sentinel | Outgoing XHR request:', { method, url, async, username, password });
-    }
+    ////// Override Network Functions /////////
 
     _overrideXMLHttpRequest(callback: (url: string, method: string) => void) {
          const originalOpen = XMLHttpRequest.prototype.open;
         XMLHttpRequest.prototype.open = function (method: string, url: string | URL, async: boolean = true, user?: string | null, password?: string | null) {
-            callback(url as string, method)
-            console.log(`Request Sentinel | [XMLHttpRequest] ${method} request to ${url}`);
+            if (callback) {
+                callback(url as string, method)
+            }
+            if (RequestSentinel.options.debug) {
+                console.log(`Request Sentinel | [XMLHttpRequest] ${method} request to ${url}`);
+            }
             return originalOpen.apply(this, arguments as any);
         }
         
-        const originalSend = XMLHttpRequest.prototype.send;
-        XMLHttpRequest.prototype.send = function (body) {
-            console.log(`Request Sentinel | [XMLHttpRequest] Request body: ${body}`);
-            return originalSend.apply(this, arguments as any);
-        };
+        // const originalSend = XMLHttpRequest.prototype.send;
+        // XMLHttpRequest.prototype.send = function (body) {
+        //     console.log(`Request Sentinel | [XMLHttpRequest] Request body: ${body}`);
+        //     return originalSend.apply(this, arguments as any);
+        // };
     }
 
-    _overrideFetch() {
+    _overrideFetch(callback: (url: string, method: string) => void) {
         const originalFetch = window.fetch;
         window.fetch = function (input, init) {
             let method = 'GET';
@@ -122,21 +119,36 @@ class RequestSentinel {
             if (init && init.method) {
                 method = init.method;
             }
-            console.log(`Request Sentinel | [fetch] ${method} request to ${url}`);
+            
             if (init && init.body) {
-                console.log(`Request Sentinel | [fetch] Request body: ${init.body}`);
+                // console.log(`Request Sentinel | [fetch] Request body: ${init.body}`);
             }
+
+            if (callback) {
+                if (RequestSentinel.options.debug) {
+                    console.log(`Request Sentinel | [fetch] ${method} request to ${url}`);
+                }
+                callback(url as string, method)
+            }
+
             return originalFetch.apply(this, arguments as any);
         };
     }
 
-    _interceptWebSocket() {
-        const originalWebSocket = window.WebSocket;
-        window.WebSocket = class extends originalWebSocket {
-            constructor(url: string | URL, protocols?: string | string[]) {
-                console.log(`Request Sentinel | [WebSocket] Connection to ${url}`);
-                super(url, protocols);
+    _interceptWebSocket(callback: (url: string, method: string) => void) {
+        const OriginalWebSocket = window.WebSocket;
+        // @ts-ignore: Overriding window.WebSocket
+        window.WebSocket = new Proxy(OriginalWebSocket, {
+            construct(target: typeof WebSocket, args: ConstructorParameters<typeof WebSocket>) {
+                const [url, protocols] = args;
+                if (RequestSentinel.options.debug) {
+                    console.log(`Request Sentinel | [WebSocket] Connection to ${url}`);
+                }
+                if (callback) {
+                    callback(url.toString(), 'WEBSOCKET');
+                }
+                return new target(url, protocols);
             }
-        };
+        });
     }
 }
